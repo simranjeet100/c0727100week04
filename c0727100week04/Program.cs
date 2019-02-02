009eeb9d79a43147e2ab0b1e4829db53570fb37b
simranjeet kaur <simie299@gmail.com>
2019-02-01 21:26:12 -0500

    simran

--- c0727100week04/Program.cs
+++ c0727100week04/Program.cs
@@ -4,32 +4,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0727100week04
+namespace c0727100
 {
     class Program
-       
     {
         static void Main(string[] args)
         {
+            Countryside c = new Countryside();
+            c.run();
+            c.travel();
+            Console.ReadLine();
 
         }
- 
-        countryside c = new countryside();
     }
-    class village
+
+    class LearningExperiment
     {
-        public bool isAstrilde;
-        public village nextVillage;
-        public  string villageName;
-    }
-    class countryside
-    {
-        village Toronto, Maple, Ajax;
         public void run()
         {
-            Maple = new village();
-            Maple.villageName = "Toronto";
-            Maple.nextVillage = Toronto;
+            Village Toronto;
+            Toronto = new Village();
+            Village a, b, c;
+            Toronto.villageName = "version A";
+            a = Toronto;
+            Console.WriteLine(a.villageName);
+            Toronto = new Village();
+            Toronto.villageName = "version B";
+            b = Toronto;
+            Console.WriteLine(b.villageName);
+            Toronto = new Village();
+            Toronto.villageName = "version C";
+            c = Toronto;
+            Console.WriteLine(c.villageName);
+
+
         }
     }
+
+
+    class Village
+    {
+        public bool isAstrildeHere;
+        public Village nextVillage;
+        public Village prevVillage;
+        public string villageName;
+    }
+
+    class Countryside
+    {
+        Village Toronto, Maple, Ajax, CurrentVillage;
+
+        public void run()
+        {
+            Maple = new Village();
+            Toronto = new Village();
+            Ajax = new Village();
+            Ajax.villageName = "Ajax";
+            Ajax.isAstrildeHere = true;
+            Maple.villageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Toronto.nextVillage = Ajax;
+            Toronto.villageName = "Toronto";
+            Ajax.nextVillage = null;
+        }
+        public void travel()
+        {
+            CurrentVillage = Maple;
+
+            while (true)
+            {
+                if (CurrentVillage.isAstrildeHere)
+                {
+                    Console.WriteLine("Astrilde is in " + CurrentVillage.villageName);
+                    Console.ReadLine();
+                }
+                else { CurrentVillage = CurrentVillage.nextVillage; }
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
 }
