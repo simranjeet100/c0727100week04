c0545c2be57e6cb3a26d74916d7e69be0ec42fd1
simranjeet kaur <simie299@gmail.com>
2019-02-01 19:18:44 -0500

    MAAN

--- c0727100week04/Program.cs
+++ c0727100week04/Program.cs
@@ -7,9 +7,29 @@ using System.Threading.Tasks;
 namespace c0727100week04
 {
     class Program
+       
     {
         static void Main(string[] args)
         {
+
+        }
+ 
+        countryside c = new countryside();
+    }
+    class village
+    {
+        public bool isAstrilde;
+        public village nextVillage;
+        public  string villageName;
+    }
+    class countryside
+    {
+        village Toronto, Maple, Ajax;
+        public void run()
+        {
+            Maple = new village();
+            Maple.villageName = "Toronto";
+            Maple.nextVillage = Toronto;
         }
     }
 }
